1391c2e3f7c20a1a363183d75b1e17eafd2bf863
ZipCPU <dgisselq@ieee.org>
2022-06-20 08:14:01 -0400

    Commented my demo file

--- sw/testfil.s
+++ sw/testfil.s
@@ -1,10 +1,10 @@
 ; Configure sensor #5
 	START
-	SEND	5,W
-	SEND	15
-	SEND	16
-	SEND	32
-	SEND	128
+	SEND	5,W	; Device address, we will be writing
+	SEND	15	; Address within sensor 5
+	SEND	16	; MEM5[15] = 16
+	SEND	32	; MEM5[16] = 32
+	SEND	128	; MEM5[17] = 128
 	STOP
 ; Configure sensor #22
 	START
@@ -18,19 +18,19 @@
 	ABORT
 ; Read from sensor 5, address 22
 	START
-	SEND	5,RD
-	SEND	22
-	START
-	SEND	5,RD
+	SEND	5,WR	; Device address, we will start out writing
+	SEND	22	; Pointer byte
+	START		; Repeated start
+	SEND	5,RD	; Device address, now we are reading
+	RXK		; Read four bytes
 	RXK
 	RXK
-	RXK
-	RXN
+	RXN		; Do not ACK the last byte
 	STOP
 	ABORT
 ; Read from sensor 22, address 22
 	START
-	SEND	22,RD
+	SEND	22,WR
 	SEND	22
 	START
 	SEND	22,RD
